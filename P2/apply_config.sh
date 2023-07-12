7451b12054ed95ac7a33b363bbae1d1a4662be1c
Raphael Gilles <rgilles@student.42.fr>
2023-07-13 00:10:42 +0200

    finished P2_A

---
+++ P2/apply_config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for cont_id in $(docker ps -q); do
+	if [[ $(docker exec $cont_id hostname | cut -d '_' -f 1) == 'router' ]]; then
+		docker cp P2/router_config.sh $cont_id:/ && \
+		docker exec $cont_id bash /router_config.sh $1
+	fi
+done
---
+++ P2/router_config.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [[ $1 == 'multi' ]]; then
+	echo UWU
+	exit 0
+else
+	if [[ $(hostname | cut -d '-' -f 2) == 1 ]]; then
+		ip link add name vxlan10 type vxlan id 10 dev eth0 \
+			remote 10.1.1.2 local 10.1.1.1 dstport 4789
+		ip addr add 10.3.1.1/24 dev vxlan10
+	else 
+		ip link add name vxlan10 type vxlan id 10 dev eth0 \
+			remote 10.1.1.1 local 10.1.1.2 dstport 4789
+		ip addr add 10.3.1.2/24 dev vxlan10
+	fi
+fi
+
+brctl addbr br0
+brctl addif br0 eth1
+brctl addif br0 vxlan10
+
+ip link set dev vxlan10 up
+ip link set dev br0 up
