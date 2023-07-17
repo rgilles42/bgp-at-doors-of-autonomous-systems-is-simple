929277a390a7aaf3070f7f4594bab317bd6da7dc
Raphael Gilles <rgilles@student.42.fr>
2023-07-17 17:00:09 +0200

    finished p2

--- P2/apply_config.sh
+++ P2/apply_config.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [[ $1 == '' ]]; then
+	echo "Please add either static or multi as argument."
+	exit 0
+fi
+
 for cont_id in $(docker ps -q); do
 	if [[ $(docker exec $cont_id hostname | cut -d '_' -f 1) == 'router' ]]; then
 		docker cp P2/router_config.sh $cont_id:/ && \
--- P2/router_config.sh
+++ P2/router_config.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 if [[ $1 == 'multi' ]]; then
-	echo UWU
-	exit 0
+	ip link add name vxlan10 type vxlan id 10 dev eth0 \
+		group 239.1.1.1 dstport 4789
+	if [[ $(hostname | cut -d '-' -f 2) == 1 ]]; then
+		ip addr add 10.3.1.1/24 dev vxlan10
+	else
+		ip addr add 10.3.1.2/24 dev vxlan10
+	fi
 else
 	if [[ $(hostname | cut -d '-' -f 2) == 1 ]]; then
 		ip link add name vxlan10 type vxlan id 10 dev eth0 \
@@ -18,5 +23,5 @@ brctl addbr br0
 brctl addif br0 eth1
 brctl addif br0 vxlan10
 
-ip link set dev vxlan10 up
 ip link set dev br0 up
+ip link set dev vxlan10 up
