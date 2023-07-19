67f7e8b9a5ebda3d1864ee6bfce1372166392616
Raphael Gilles <rgilles@student.42.fr>
2023-07-19 16:50:51 +0200

    build_images now launches from project root

--- P1/build_images.sh
+++ P1/build_images.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t host_rgilles - < ./host_rgilles_Dockerfile
-docker build -t router_rgilles - < ./router_rgilles_Dockerfile
+docker build -t host_rgilles - < ./P1/host_rgilles_Dockerfile
+docker build -t router_rgilles - < ./P1/router_rgilles_Dockerfile
